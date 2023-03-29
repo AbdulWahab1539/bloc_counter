3b219d98f301265064b2cdafdb7716272c499b03
abdulwahab1539 <abdulwahab1539@gmail.com>
2023-03-29 15:25:50 +0500

    BlocCommunication using BlocListener instead of StreamsSubscriptions

--- lib/presentation/screen/home_screen.dart
+++ lib/presentation/screen/home_screen.dart
@@ -22,117 +22,118 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return BlocListener<InternetCubit, InternetState>(
-      listener: (context, state) {
-        if (state is InternetConnected &&
-            state.connectionType == ConnectionType.wifi) {
-          context.read<CounterCubit>().increment();
-          // return const Text('Wifi');
-        } else if (state is InternetConnected &&
-            state.connectionType == ConnectionType.mobileData) {
-          context.read<CounterCubit>().decrement();
-          // return const Text('Mobile Data');
-        } else if (state is InternetDisonnected) {
-          // return const Text('Disconnected');
-        }
-        // return const CircularProgressIndicator();
-      },
-      child: Scaffold(
-        appBar: AppBar(
-          // foregroundColor: widget.color,
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              BlocBuilder<InternetCubit, InternetState>(
-                  builder: (context, state) {
-                if (state is InternetConnected &&
-                    state.connectionType == ConnectionType.wifi) {
-                  return const Text('Wifi');
-                } else if (state is InternetConnected &&
-                    state.connectionType == ConnectionType.mobileData) {
-                  return const Text('Mobile Data');
-                } else if (state is InternetDisonnected) {
-                  return const Text('Disconnected');
-                }
-                return const CircularProgressIndicator();
-              }),
-              BlocConsumer<CounterCubit, CounterState>(
-                listener: (context, state) {
-                  if (state.isIncremented) {
-                    const snackbar = SnackBar(
-                      content: Text('Hurrayyy'),
-                      duration: Duration(milliseconds: 300),
-                    );
-                    ScaffoldMessenger.of(context).showSnackBar(snackbar);
-                  }
-                },
+    return Scaffold(
+      appBar: AppBar(
+        // foregroundColor: widget.color,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            BlocBuilder<InternetCubit, InternetState>(
                 builder: (context, state) {
-                  print(state.counterValue);
-                  return Text(
-                    // '$_counter',
-                    '${state.counterValue}',
-                    style: Theme.of(context).textTheme.headline4,
+              if (state is InternetConnected &&
+                  state.connectionType == ConnectionType.wifi) {
+                return const Text('Wifi');
+              } else if (state is InternetConnected &&
+                  state.connectionType == ConnectionType.mobileData) {
+                return const Text('Mobile Data');
+              } else if (state is InternetDisonnected) {
+                return const Text('Disconnected');
+              }
+              return const CircularProgressIndicator();
+            }),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.isIncremented) {
+                  const snackbar = SnackBar(
+                    content: Text('Hurrayyy'),
+                    duration: Duration(milliseconds: 300),
                   );
-                },
-              ),
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.center,
-              //   children: [
-              //     FloatingActionButton(
-              //       onPressed: () {
-              //         BlocProvider.of<CounterCubit>(context).increment();
-              //         // context.bloc<CounterCubit>().increment();
-              //       },
-              //       tooltip: 'Increment',
-              //       child: const Icon(Icons.add),
-              //     ),
-              //     const SizedBox(
-              //       width: 10,
-              //     ),
-              //     FloatingActionButton(
-              //       onPressed: () {
-              //         BlocProvider.of<CounterCubit>(context).decrement();
-              //       },
-              //       tooltip: 'Decrement',
-              //       child: const Icon(Icons.remove),
-              //     ),
-              //   ],
-              // ),
-              const SizedBox(
-                height: 20,
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/second');
-                  // MaterialPageRoute(
-                  //     builder: (_) => BlocProvider.value(
-                  //           value: BlocProvider.of<CounterCubit>(context),
-                  //           child: const SecondScreen(
-                  //               title: 'Seond Screen', color: Colors.purple),
-                  //         ));
-                },
-                color: Colors.yellow,
-                child: const Text('Go to Second Screen'),
-              ),
-              MaterialButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/third');
-                  // MaterialPageRoute(
-                  //     builder: (_) => BlocProvider.value(
-                  //           value: BlocProvider.of<CounterCubit>(context),
-                  //           child: const SecondScreen(
-                  //               title: 'Seond Screen', color: Colors.purple),
-                  //         ));
-                },
-                color: Colors.orange,
-                child: const Text('Go to Third Screen'),
-              )
-            ],
-          ),
+                  ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                }
+              },
+              builder: (context, state) {
+                print(state.counterValue);
+                return Text(
+                  // '$_counter',
+                  '${state.counterValue}',
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            Builder(builder: (context) {
+              final internetState = context.watch<InternetCubit>().state;
+              final counterState = context.watch<CounterCubit>().state;
+
+              if (internetState is InternetConnected &&
+                  internetState.connectionType == ConnectionType.wifi) {
+                return Text(
+                    'Counter: ${counterState.counterValue} Internet: ${internetState.connectionType}');
+              } else if (internetState is InternetConnected &&
+                  internetState.connectionType == ConnectionType.mobileData) {
+                return Text(
+                    'Counter: ${counterState.counterValue} Internet: ${internetState.connectionType}');
+              } else {
+                return Text(
+                    'Counter: ${counterState.counterValue} Internet: Disconnected');
+              }
+            }),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                    // context.bloc<CounterCubit>().increment();
+                  },
+                  tooltip: 'Increment',
+                  child: const Icon(Icons.add),
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/second');
+                // MaterialPageRoute(
+                //     builder: (_) => BlocProvider.value(
+                //           value: BlocProvider.of<CounterCubit>(context),
+                //           child: const SecondScreen(
+                //               title: 'Seond Screen', color: Colors.purple),
+                //         ));
+              },
+              color: Colors.yellow,
+              child: const Text('Go to Second Screen'),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed('/third');
+                // MaterialPageRoute(
+                //     builder: (_) => BlocProvider.value(
+                //           value: BlocProvider.of<CounterCubit>(context),
+                //           child: const SecondScreen(
+                //               title: 'Seond Screen', color: Colors.purple),
+                //         ));
+              },
+              color: Colors.orange,
+              child: const Text('Go to Third Screen'),
+            )
+          ],
         ),
       ),
     );
